c1f045f180608f8231267729b68b99d73ed20e42
Robert Nevitt <gitcommits@robertnevitt.com>
2020-05-26 10:41:27 +0200

    Added vertx web and jdbc handling

--- redeploy.sh
+++ redeploy.sh
@@ -8,6 +8,7 @@ export VERTX_CMD="run"
 mvn compile dependency:copy-dependencies
 java \
   -cp  $(echo target/dependency/*.jar | tr ' ' ':'):"target/classes" \
+  -Dhsqldb.reconfig_logging=false \
   $LAUNCHER $VERTX_CMD $VERTICLE \
   --redeploy="src/main/**/*" --on-redeploy="$CMD" \
   --launcher-class=$LAUNCHER \
